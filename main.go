1f76b88c1b5ef016efdbfedcf58bb1a0b139e696
GlebZigert <IforgettheGithub123456>
2021-12-17 14:05:13 +0300

    Presenting the user data

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"	
 )
 
 // templ represents a single template
@@ -29,7 +30,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.Must(template.ParseFiles(filepath.Join("templates",
 				t.filename)))
 	})
-	t.templ.Execute(w, r)
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+		}
+		if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+		}
+		fmt.Println("data[UserData]", data["UserData"])
+		
+
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
