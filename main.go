ebd4c36fb122430ca2eaa5d1503c7da3c4a1d564
GlebZigert <IforgettheGithub123456>
2021-12-17 13:51:02 +0300

    Logging in Handling the response from the provider

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"log"
+	"flag"
 	"fmt"
+	"gochat/trace"
+	"log"
 	"net/http"
 	"path/filepath"
 	"sync"
 	"text/template"
-	"gochat/trace"
-	"flag"
-
-
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templ represents a single template
@@ -19,9 +21,6 @@ type templateHandler struct {
 	filename string
 	templ    *template.Template
 }
-
-
-
 
 // ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -33,21 +32,33 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, r)
 }
 
-
-
 func main() {
 
 	fmt.Println(trace.ReverseRunes(".olleH"))
 
-	var addr=flag.String("addr",":8080","the addr of the application.")
+	var addr = flag.String("addr", ":8080", "the addr of the application.")
 
 	flag.Parse()
 
+	// set up gomniauth
+	gomniauth.SetSecurityKey("some long key")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret",
+			"http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret",
+			"http://localhost:8080/auth/callback/github"),
+		google.New("234438523275-p23ad7cql6c66mpdsq0d2097ei9smv7d.apps.googleusercontent.com", 
+		"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",
+			"http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
-//	r.tracer = trace.New(os.Stdout)
-//	http.Handle("/", &templateHandler{filename: "chat.html"})
+	//	r.tracer = trace.New(os.Stdout)
+	//	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
@@ -57,5 +68,5 @@ func main() {
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-	
+
 }
